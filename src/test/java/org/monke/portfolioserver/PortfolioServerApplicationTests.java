0daf08319acafbc34c86e483c990514a64a32272
UnknownMonke <cdeg09@gmail.com>
2024-05-24 20:38:21 +0200

    init springboot project

---
+++ src/test/java/org/monke/portfolioserver/PortfolioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.monke.portfolioserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
